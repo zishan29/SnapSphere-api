6332084b23af69db87c712b638ff9818a8374ee4
Zishan Shaikh <100702557+zishan29@users.noreply.github.com>
2024-01-30 19:03:52 +0530

    Add cors origin for vercel

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ require('dotenv').config();
 require('./passport/passport');
 
 const express = require('express');
-const path = require('path');
 const cookieParser = require('cookie-parser');
-const logger = require('morgan');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const cloudinary = require('cloudinary').v2;
@@ -24,23 +22,16 @@ mongoose.connect(mongoDb);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'mongo connection error'));
 
-const corsOptions = {
-  origin: [
-    'https://snaptalk-411413.web.app',
-    'https://snaptalk-411413.firebaseapp.com',
-    'http://localhost:3000',
-  ],
-  optionsSuccessStatus: 200,
-};
-
-app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(
+  cors({
+    origin: 'https://snapshpere.vercel.app',
+    credentials: true,
+  }),
+);
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: false }));
 
-app.options('*', cors(corsOptions));
-
-app.use('/', cors(corsOptions), indexRouter);
+app.use('/', indexRouter);
 
 module.exports = app;
